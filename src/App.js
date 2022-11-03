76f6be447fdfcc04af3284cef52ab9191515083c
Rebecca Barrett <barrett4467@gmail.com>
2022-11-03 12:35:28 -0600

    adding browser router and setting up routes/basic components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Services from "./components/Services";
+import Previous from "./components/Previous";
+import Contact from "./components/Contact";
 
-function App() {
+
+
+const App = () => {
+  useEffect(() => {
+    document.title = "Sentimetal"
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/services" element={<Services/>} />
+        <Route path="/previous-works" element={<Previous/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+
+        {/* Home, Contact, Previous works, order metal tiles, Products page */}
+      </Routes>
+    
+    
+    </BrowserRouter>
+
   );
 }
 
---
+++ src/components/Previous.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Previous = () => {
+    return (
+        <h1>Previous</h1>
+    )
+}
+
+export default Previous;
