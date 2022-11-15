cec78e9ff9eba4e89bb4b64fe4611e0148ca1a5a
Rebecca Barrett <barrett4467@gmail.com>
2022-11-14 20:35:50 -0700

    basic styling added to navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,22 @@ const Home = () => {
     return (
         <>
             <Navbar />
-            <h1>Home</h1>
+            <h1>Sentimetal Welding & Fabrication</h1>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut sem sem. Nunc vitae massa tortor. Cras luctus pulvinar libero, et posuere magna suscipit vitae. Sed nec odio imperdiet, luctus justo eget, tempor mi. Proin ut vehicula augue, eget commodo lorem. Vestibulum rutrum justo dui, quis maximus dolor tincidunt eget. Quisque purus mi, pharetra eget magna vitae, mattis hendrerit eros. Duis quis nulla odio. Quisque scelerisque non elit at molestie.
+            </p>
+            <p>
+            Nam tincidunt luctus ipsum, a congue est ullamcorper nec. Proin lobortis gravida magna vitae viverra. Sed a vulputate est, laoreet vestibulum erat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce quis malesuada lectus. Maecenas pharetra porttitor porta. Duis ipsum ex, vestibulum eget urna in, tempus tincidunt enim. Sed sit amet dapibus lacus. Quisque at nisl purus. Proin commodo magna ac congue cursus.
+            </p>
+            <p>
+            Phasellus vel nisl ut nisi consequat tempus. Fusce rhoncus justo ut ultrices bibendum. In hac habitasse platea dictumst. Fusce aliquet, dui id vulputate gravida, nisi magna rutrum neque, ut eleifend ipsum magna at leo. Aenean vehicula sagittis nulla vel consectetur. Sed at consequat ante, id fermentum tortor. Pellentesque pharetra rhoncus tellus vitae euismod. In consectetur non eros nec imperdiet.
+            </p>
+            <p>
+            Donec luctus odio at pretium congue. Ut vel erat felis. Integer molestie ex eget scelerisque egestas. Nam a accumsan turpis, id fringilla felis. Etiam molestie mi eget velit ullamcorper, vel tristique nibh convallis. Nullam nec enim sed felis tempus tincidunt. Suspendisse venenatis, nisl ac fermentum consectetur, libero quam condimentum neque, vel dignissim magna leo eget turpis. Nam at imperdiet enim. Donec ac est fringilla, semper enim sed, sollicitudin lorem. Vivamus in gravida dolor. Donec congue ligula magna, sit amet efficitur libero blandit vel. Aenean vitae condimentum ligula, nec rhoncus nulla. Integer convallis rhoncus diam, facilisis blandit mi pretium et. Curabitur laoreet turpis non ante fringilla, a viverra justo luctus.
+            </p>
+            <p>
+            Quisque non sodales mauris. Nunc eget purus nec arcu semper efficitur vitae et nisl. Nunc vel congue ipsum. Cras quis elit nibh. Cras sodales tellus lorem. Praesent faucibus sit amet enim at eleifend. Ut velit lorem, luctus vitae efficitur at, porttitor laoreet magna. Phasellus luctus, leo non efficitur pretium, mauris odio interdum tortor, eget tincidunt arcu mi vitae ipsum. Phasellus cursus justo eu enim euismod, id vehicula ante pharetra. Aenean sed eleifend justo.
+            </p>
         </>
     )
 }
--- src/elements/Navbar.js
+++ src/elements/Navbar.js
@@ -1,14 +1,63 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {createGlobalStyle} from "styled-components";
+import styled from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+`
+const Nav = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px;
+`
+const DropDownContent = styled.div`
+    display: none;
+    position: absolute;
+    min-width: 160px;
+    z-index: 1;
+`
+const StyledLink = styled(Link)`
+    position: relative;
+    top: 10px;
+    text-decoration: none;
+    color: #3b0512;
+    font-size: 1.5em;
+    letter-spacing: 2px;
+    margin: 0 auto;
+    &:hover {
+        color: #dfac7a;
+    }
+    &:hover ${DropDownContent} {
+        display: block;
+    }
+`
+const Menu = styled.div`
+    display: flex;
+    gap: 1em;
+    font-size: 18px;
+`
+
 
 function Navbar() {
     return(
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/services">Services</Link>
-            <Link to="/previous-work">Previous Work</Link>
-            <Link to="/contact">Contact Us</Link>
-        </div>
+        <>
+        <GlobalStyle />
+        <Nav>
+            <div>Logo</div>
+            <Menu>
+                <StyledLink to="/">Home</StyledLink>
+                <StyledLink to="/services">Services</StyledLink>
+                <StyledLink to="/previous-work">Previous Work</StyledLink>
+                <StyledLink to="/contact">Contact Us</StyledLink>
+            </Menu>
+        </Nav>
+        </>
     )
 }
 
